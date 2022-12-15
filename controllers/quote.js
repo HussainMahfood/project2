ad4f5a4e190ab1ce529a641d2d75910724471e9d
bader-abdulla <bad.abd@outlook.com>
2022-12-15 16:21:13 +0300

    quote BE

---
+++ controllers/quote.js
@@ -0,0 +1,67 @@
+// Require Model
+const Quote = require("../models/Quote");
+// const {Car} = require("../models/Car");
+
+
+
+// add
+exports.quote_add_post = (req, res) => {
+    let quote = new Quote (req.body)
+    quote.save()
+    .then(()=>{
+        res.send("new data added");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+// list
+exports.quote_list_get = (req, res) => {
+    Quote.find()
+    .then(quote => {
+    res.json(quote);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+
+
+// view
+exports.quote_view_get = (req, res) => {
+    Quote.findById(req.params.id)
+    .then(quote => {
+        res.json(quote);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+
+
+// update
+exports.quote_update_post = (req, res) => {
+    Quote.findByIdAndUpdate(req.params.id , req.body)
+    .then(() => {
+        res.send ('data updated')
+    })
+    .catch(err => {
+        console.log(err)
+    });
+}
+
+
+
+// delete
+exports.quote_delete_get = (req, res) => {  
+    Quote.findByIdAndDelete(req.params.id)
+    .then(quote => {
+        res.send("record deleted");
+    })
+    .catch(err => {
+        console.log(err);
+    })
+};
---
+++ routes/quote.js
@@ -0,0 +1,40 @@
+// Require Express
+const express = require ('express');
+
+// Initialize Router functionality
+const router = express.Router();
+
+router.use(express.urlencoded({ extended: true }));
+
+// Require isLoggedIn
+const isLoggedIn = require ('../helper/isLoggedIn');
+
+
+
+//// Controllers
+const quoteCtrl = require("../controllers/quote");
+
+
+
+//// Routes
+
+// add
+// router.post("/quote/add", isLoggedIn, quoteCtrl.quote_add_post);
+router.post("/quote/add", quoteCtrl.quote_add_post);
+
+// list
+router.get("/quote/list", quoteCtrl.quote_list_get);
+
+// view
+router.get("/quote/view/:id", quoteCtrl.quote_view_get);
+
+// update
+router.post("/quote/update/:id", quoteCtrl.quote_update_post);
+
+// delete
+router.get("/quote/delete/:id", quoteCtrl.quote_delete_get);
+
+
+
+// Exports
+module.exports = router;
