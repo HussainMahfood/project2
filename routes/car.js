8c660e4c6195db4151807adcf5155641ba25ec70
mahfood <hussain@mahfood.ws>
2022-12-20 10:36:48 +0300

    ver 2 dev

--- routes/car.js
+++ routes/car.js
@@ -16,8 +16,8 @@ router.post("/car/add", carCntrl.car_add_post);
 router.get("/car/delete/:id", carCntrl.car_delete_get);
 router.get("/car/list", carCntrl.car_list_get);
 
-router.get("/car/view", carCntrl.car_view_get);
-router.post("/car/update", carCntrl.car_update_post);
+router.get("/car/view/:id", carCntrl.car_view_get);
+router.post("/car/update/:id", carCntrl.car_update_post);
 
 
 module.exports = router;
