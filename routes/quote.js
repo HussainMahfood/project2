4d6b3d2a16fc5d8ef4b2308a9383e1d63bcbbe3d
bader-abdulla <bad.abd@outlook.com>
2022-12-20 12:08:29 +0300

    Quote changes done

--- routes/quote.js
+++ routes/quote.js
@@ -14,21 +14,24 @@ const quoteCtrl = require("../controllers/quote");
 
 
 // Routes
-// router.post("/quote/add", isLoggedIn, quoteCtrl.quote_add_post);
+
 // add
+// router.get("/quote/add", isLoggedIn, quoteCtrl.quote_add_get);
+router.get("/quote/add", quoteCtrl.quote_add_get);
 router.post("/quote/add", quoteCtrl.quote_add_post);
 
 // list
 router.get("/quote/list", quoteCtrl.quote_list_get);
 
 // view
-router.get("/quote/view/:id", quoteCtrl.quote_view_get);
+router.get("/quote/view", quoteCtrl.quote_view_get);
 
 // update
-router.post("/quote/update/:id", quoteCtrl.quote_update_post);
+router.get("/quote/edit", quoteCtrl.quote_edit_get);
+router.post("/quote/update", quoteCtrl.quote_update_post);
 
 // delete
-router.get("/quote/delete/:id", quoteCtrl.quote_delete_get);
+router.get("/quote/delete", quoteCtrl.quote_delete_get);
 
 
 // Exports
